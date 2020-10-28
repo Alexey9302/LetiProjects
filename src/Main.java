cdff65e4b3491bb40bb2f373b9179f8c1caedbeb
Alexey Bikchurin <bikchurin2001@mail.ru>
2020-10-28 11:36:38 +0300

    Add lab 3.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
-import binaryheap.tree.BinaryHeap;
-import binaryheap.tree.DfsIterator;
-
 public class Main {
     public static void main(String... args){
-        BinaryHeap binaryHeap = new BinaryHeap();
-        binaryHeap.insert(1);
-        binaryHeap.insert(5);
-        binaryHeap.insert(3);
-        binaryHeap.insert(9);
-        binaryHeap.insert(2);
 
-        DfsIterator iterator = binaryHeap.getDfsIterator();
-        while(iterator.hasNext()){
-            iterator.next();
-            iterator.remove();
-        }
     }
 }
